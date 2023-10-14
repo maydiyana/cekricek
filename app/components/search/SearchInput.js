619a45fc6c82ab2fcce6901485600b85777d251c
maydiyana <144259593+maydiyana@users.noreply.github.com>
2023-10-14 18:14:50 +0700

    Update SearchInput.js

--- app/components/search/SearchInput.js
+++ app/components/search/SearchInput.js
@@ -11,14 +11,18 @@ const SearchInput = () => {
     const router = useRouter()
 
     const handleSearch = (event) => {
-        event.preventDefault()
-        const keyword = searchRef.current.value
+
+            if(event.key === "Enter" || event.type === "click") {
+                event.preventDefault()
+
+                const keyword = searchRef.current.value
         
-        router.push(`/search/${keyword}`)
+                router.push(`/cari/${keyword}`)
+            }
     }
     return(
     <div className="relative ">
-        <input className="rounded w-full rounded-md p-1" placeholder="Search..." ref={searchRef}/>
+        <input className="rounded w-full rounded-md p-1" placeholder="Search..." ref={searchRef} onKeyDown={handleSearch}/>
         <button className="absolute top-1 end-2 font-serif text-slate-500 rounded" onClick={handleSearch}>Find</button>
     </div>
     )
