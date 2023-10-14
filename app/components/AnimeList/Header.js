e9f1598923ef1a6acb2ab4e3b25516b328622584
maydiyana <144259593+maydiyana@users.noreply.github.com>
2023-10-14 18:16:32 +0700

    Update Header.js

--- app/components/AnimeList/Header.js
+++ app/components/AnimeList/Header.js
@@ -4,7 +4,10 @@ const Header = ({ title, linkHref, linkTitle }) => {
     return (
         <div className="flex justify-between p-4 ">
       <h1 className="font-bold">{title}</h1>
-      <Link className="underline" href={linkHref}>{linkTitle}</Link>
+      {linkHref && linkTitle ? 
+            <Link className="underline" href={linkHref}>{linkTitle}</Link>
+            : null
+      }
       
       </div>
     )
