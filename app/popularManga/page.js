6fb756540e476477d7edac387a846088b6da3d12
maydiyana <144259593+maydiyana@users.noreply.github.com>
2023-10-14 18:58:47 +0700

    Update page.js

--- app/popularManga/page.js
+++ app/popularManga/page.js
@@ -5,12 +5,12 @@ import MangaList from "../components/AnimeList/mangaList"
 export default async function Home() {
 
 const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/manga`)
-   const topAnime = await response.json()
+   const topManga = await response.json()
 
   return (
     <section>
-      <Header title="Most Popular" linkTitle="See All" linkHref="/popular" /> 
-      <MangaList api={topAnime} />
+      <Header title="Most Popular" linkTitle="See All" linkHref="/popularManga" /> 
+      <MangaList api={topManga} />
     </section>
       
     
