b8a5093c612f672f9af4611e53fde03e06604981
maydiyana <144259593+maydiyana@users.noreply.github.com>
2023-10-19 23:03:52 +0700

    Update index.js

--- app/components/utilities/index.js
+++ app/components/utilities/index.js
@@ -1,14 +1,18 @@
 import SearchInput from "../search/SearchInput"
 
+import { Providers } from "@/app/providers"
+import ThemeChanger from "@/app/themeCahnger"
+
 
 const { default: Link } = require("next/link")
 
 const Navbar = () => {
     return(
         
-    <header className="bg-indigo-300 rounded">
-    <div className="flex md:flex-row flex-col justify-between p-3 gap-2">
+    <header className="bg-indigo-300 rounded ">
+    <div className="flex md:flex-row justify-between p-3 gap-2">
         <Link className="font-bold text-2xl text-slate-900 font-mono text-white " href="/">WIBU LIST</Link>
+        <Providers><ThemeChanger/></Providers>
         <SearchInput />
     </div>
     </header>
