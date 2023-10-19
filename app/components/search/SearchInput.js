d4533c846309237eebb8ec9f8c3086860a1ca956
maydiyana <144259593+maydiyana@users.noreply.github.com>
2023-10-19 23:03:28 +0700

    Update SearchInput.js

--- app/components/search/SearchInput.js
+++ app/components/search/SearchInput.js
@@ -4,27 +4,28 @@ import { useRouter } from "next/navigation"
 import { useRef } from "react"
 
 
-
-
 const SearchInput = () => {
     const searchRef = useRef()
     const router = useRouter()
 
     const handleSearch = (event) => {
+        const keyword = searchRef.current.value
+
+        if(!keyword) return
 
             if(event.key === "Enter" || event.type === "click") {
                 event.preventDefault()
 
-                const keyword = searchRef.current.value
         
                 router.push(`/cari/${keyword}`)
             }
     }
     return(
-    <div className="relative ">
-        <input className="rounded w-full rounded-md p-1" placeholder="Search..." ref={searchRef} onKeyDown={handleSearch}/>
-        <button className="absolute top-1 end-2 font-serif text-slate-500 rounded" onClick={handleSearch}>Find</button>
-    </div>
+        <div class="search-box">
+    <button class="btn-search pb-2"><ion-icon name="search-outline"  onClick={handleSearch}></ion-icon></button>
+    <input type="text" class="input-search" placeholder="Type to Search..." ref={searchRef} onKeyDown={handleSearch}/>
+  </div>
+
     )
 }
 
